229f2598e215b502397f5616bddf8518ae720ff1
wcvaughan <chrisvaughanlds@gmail.com>
2025-03-14 13:24:31 -0800

    regenerated swagger.json

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.use('/api-docs', swaggerUi.setup(swaggerDocument));
 
+router.use('/authRoutes', require('./authRoutes'));
+router.use('/customers', require('./customers'));
+router.use('/loginsignup', require('./loginsignup'));
 router.use('/transactions', require('./transactions'));
 router.use('/users', require('./users'));
 
